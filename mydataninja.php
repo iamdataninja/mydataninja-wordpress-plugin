85a881027142f35321ea4ec9d3124b10b6746d6e
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2024-01-13 15:34:08 +0400

    added newer version

--- mydataninja.php
+++ mydataninja.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: MyDataNinja - Ad Performance Tracking, Order Reports, CRM, Analytics, and Optimization Tools
 Description: One, Easy Solution to Create, Track and Optimize Digital Marketing Campaigns.
-Version: 1.0
+Version: 1.0.1
 Author: <a href="https://mydataninja.com/">MyDataNinja</a>
 Tags: woocommerce, e-commerce, google ads, facebook ads, meta ads, ppc, digital advertising, sales, sales management, business analytics, woocommerce, e-commerce, google ads, facebook ads, digital advertising
 License: GPLv3
--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 
 Tags: woocommerce, e-commerce, google ads, facebook ads, digital advertising
 Requires at least: 6.0
-Tested up to: 6.4
+Tested up to: 6.5
 Requires PHP: 7.4
-Stable tag: 1.0
+Stable tag: 1.0.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
