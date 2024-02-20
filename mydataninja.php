874194d13a748bbcbed0e4cf98fcd1f4516362e7
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2024-02-20 19:43:09 +0400

    Changed plugin information

--- mydataninja.php
+++ mydataninja.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: MyDataNinja WooCommerce
-Description: Effortlessly track and manage profits for your products.
+Plugin Name: MyDataNinja - Ad Performance Tracking, Order Reports, CRM, Analytics, and Optimization Tools
+Description: One, Easy Solution to Create, Track and Optimize Digital Marketing Campaigns.
 Version: 1.0
 Author: <a href="https://github.com/Afcyy">Vazha Aptsiauri</a> | <a href="https://mydataninja.com/">MyDataNinja</a>
-Tags: woocommerce, e-commerce, profit, profit tracking, sales, sales management, business analytics
+Tags: woocommerce, e-commerce, google ads, facebook ads, meta ads, ppc, digital advertising, sales, sales management, business analytics
 */
 
 require __DIR__ . '/vendor/autoload.php';
