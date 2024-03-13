86da22708981903d81b5b8bba9a658386f80b9b7
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2024-03-13 14:08:17 +0400

    added rest files to includes

--- includes.php
+++ includes.php
@@ -6,3 +6,4 @@ include_once __DIR__ . '/src/Script/AddScript.php';
 include_once __DIR__ . '/src/Script/SendLastEventId.php';
 include_once __DIR__ . '/src/Interface/PluginInterface.php';
 include_once __DIR__ . '/src/AccessToken/SaveAccessToken.php';
+include_once __DIR__ . '/src/REST/endpoints.php';
