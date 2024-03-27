9cdd8d24178397ff618316b50a1acd5939ff3162
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2024-03-27 12:59:51 +0400

    Refactor endpoints

--- src/Script/AddScript.php
+++ src/Script/AddScript.php
@@ -2,33 +2,6 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function mdnj_attach_website_route_callback($request) {
-    $parameters = $request->get_params();
-
-    $consumer_secret = $request->get_param('consumer_secret');
-    if (!mdnj_check_user($consumer_secret)) {
-        return new WP_Error('invalid_consumer_key_or_secret', 'Invalid consumer key or secret', array('status' => 403));
-    }
-
-    $website_id = isset($parameters['website_id']) ? $parameters['website_id'] : '';
-
-    if (!empty($website_id)) {
-        update_option('dataninja_website_id', $website_id);
-        return rest_ensure_response(array('success' => true));
-    } else {
-        return new WP_Error('missing_website_id', 'Website ID is missing', array('status' => 400));
-    }
-}
-
-function mdnj_create_attach_website_endpoint() {
-    register_rest_route('mydataninja/v1', '/attach-website', array(
-        'methods' => 'POST',
-        'callback' => 'mdnj_attach_website_route_callback',
-    ));
-}
-
-add_action('rest_api_init', 'mdnj_create_attach_website_endpoint');
-
 function mdnj_check_user($consumer_secret_substr) {
   global $wpdb;
   $prefix = 'MyDataNinja - API';
@@ -80,33 +53,3 @@ function mdnj_add_ninja_script() {
 }
 
 add_action('wp_footer', 'mdnj_add_ninja_script');
-
-function mdnj_save_form_hash_callback($request) {
-  $parameters = $request->get_params();
-
-  $form_hash = isset($parameters['hash']) ? $parameters['hash'] : '';
-  $form_id = isset($parameters['form_id']) ? $parameters['form_id'] : '';
-
-  if (!empty($form_hash)) {
-    update_option('mdnj_form_hash', $form_hash);
-  }
-
-  if (!empty($form_id)) {
-    update_option('mdnj_form_id', $form_id);
-  }
-
-  if (!empty($form_hash) || !empty($form_id)) {
-    return rest_ensure_response(array('success' => true));
-  } else {
-    return new WP_Error('missing_form_hash_or_form_id', 'Form Hash or Form ID is missing', array('status' => 400));
-  }
-}
-
-function mdnj_create_save_form_hash_endpoint() {
-  register_rest_route('mydataninja/v1', '/save-hash', array(
-    'methods' => 'POST',
-    'callback' => 'mdnj_save_form_hash_callback',
-  ));
-}
-
-add_action('rest_api_init', 'mdnj_create_save_form_hash_endpoint');
