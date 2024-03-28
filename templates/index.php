1c9099e9dc686063f432452a83e1387b1c90bdac
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2024-03-28 15:25:26 +0400

    Replace .save-btn with .mdnj-save-btn

--- templates/index.php
+++ templates/index.php
@@ -57,7 +57,7 @@ $meta_keys = $wpdb->get_col("
                     echo '<button class="mdnj-btn mdnj-authorize-btn" disabled>Authorized Successfully</button>';
                     echo '<a href="' . esc_url($myDataNinjaConfig['FRONT_BASE_URL']) . '/dashboard" class="mdnj-btn mdnj-authorize-btn">Open MyDataNinja</a>';                  } else {
                     if ($is_plain_permalinks) {
-                      echo '<a href="' . esc_url(admin_url('options-permalink.php')) . '" class="mdnj-btn mdnj-authorize-btn save-btn no-underline">Adjust Permalink Settings</a>';
+                      echo '<a href="' . esc_url(admin_url('options-permalink.php')) . '" class="mdnj-btn mdnj-authorize-btn mdnj-save-btn no-underline">Adjust Permalink Settings</a>';
                     } else {
                       echo '<a href="' . esc_url($myDataNinjaConfig['FRONT_BASE_URL']) . '/crm/woocommerce?name=' . esc_html(get_bloginfo('name')) . '&currency=' . esc_html(get_woocommerce_currency()) . '&base_url=' . esc_url(home_url()) . '" class="mdnj-btn mdnj-authorize-btn">Authorize</a>';
                     }
@@ -119,7 +119,7 @@ $meta_keys = $wpdb->get_col("
                               </div>
                           </div>
 
-                          <input type="submit" class="mdnj-btn save-btn" style="font-size: 14px; width: 80%" value="Save Changes">
+                          <input type="submit" class="mdnj-btn mdnj-save-btn" style="font-size: 14px; width: 80%" value="Save Changes">
                       </div>
                   <?php endif; ?>
 
