2afd2bfb0efed2990ce0c8493083867201614933
malkhazidartsmelidze <malkhazidartsmelidze@gmail.com>
2024-10-01 20:02:10 +0400

    fixed profit

--- src/CostOfGoods/AddCogAndProfitToOrders.php
+++ src/CostOfGoods/AddCogAndProfitToOrders.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (! defined('ABSPATH')) exit;
 
 global $mdnj_cog_field_name;
 $mdnj_cog_field_name = get_option('mdnj_existing_cog_field_name', 'mdnj_cost_of_goods');
@@ -42,14 +42,14 @@ function mdnj_calculate_order_profit($order_id)
     $product = wc_get_product($product_id);
     $price = $product ? (float)$product->get_price() : 0;
 
-    $include_profits = get_option('mdnj_include_profits', 'no');
-    if($include_profits == 'yes'){
-      if (!empty($cost_of_goods) && $cost_of_goods > 0) {
-        $profit_fixed = $price - $cost_of_goods;
-      } else {
-        $profit_fixed = ($price * $default_profit_margin / 100);
-      }
+    // $include_profits = get_option('mdnj_include_profits', 'no');
+    // if($include_profits == 'yes'){
+    if (!empty($cost_of_goods) && $cost_of_goods > 0) {
+      $profit_fixed = $price - $cost_of_goods;
+    } else {
+      $profit_fixed = ($price * $default_profit_margin / 100);
     }
+    // }
 
     $total_profit += $profit_fixed ?? 0;
   }
@@ -57,7 +57,8 @@ function mdnj_calculate_order_profit($order_id)
   return $total_profit;
 }
 
-function mdnj_add_profit_and_cost_of_goods_to_order_response($response, $object) {
+function mdnj_add_profit_and_cost_of_goods_to_order_response($response, $object)
+{
   $order_id = $object->get_id();
   $response->data['mydataninja_profit'] = mdnj_calculate_order_profit($order_id);
   $response->data['mydataninja_cost_of_goods'] = mdnj_calculate_order_cost_of_goods($order_id);
